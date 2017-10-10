9ca771232f6b69397ca57c32df450539bf72f546
Ryan Impey <ryanimpey@live.co.uk>
2017-10-10 15:49:42 +0100

    Router + Setup changes
    Added Bulma/Buefy. Added history mode to router. Added Font Awesome

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// Absolute Imports
+import 'buefy/lib/buefy.css';
+import Buefy from 'buefy';
 import Vue from 'vue';
+
+// Relative Imports
+import '../static/font-awesome/css/font-awesome.css';
+
 import App from './App';
 import router from './router';
+
+
+Vue.use(Buefy, {
+  defaultIconPack: 'fa',
+});
 
 Vue.config.productionTip = false;
 
