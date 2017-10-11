ebf72864c2445e4a6eeb85b02f58a713a5e880be
Ryan Impey <ryanimpey@live.co.uk>
2017-10-11 15:38:17 +0100

    Set up Async for use later.
    Renamed function for getting steam ID and added async for data loading.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,37 +1,56 @@
 <template>
-  <div class="Profile">
-    <h1>{{ steamUserID }} </h1>
+  <div class="Profile column">
+    <div class="columns">
+      <div class="column is-2 is-offset-4 profile-text">
+        <p class="title is-4">{{ this.$route.params.username }} </p>
+        <p class="subtitle is-6">{{ steamUserID }} </p>
+      </div>
+      <div class="column is-2">
+        <img src="http://placehold.it/300x300" class="profile-avatar is-pulled-right">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import async from 'async';
 import steamAPIKey from '../assets/keys';
 
 export default {
   name: 'Profile',
   beforeMount() {
-    this.getSteamID();
+    async.series([
+      (callback) => {
+        callback(null, 'Get ID');
+        this.getSteamID();
+      },
+      (callback) => {
+        callback(null, '2');
+      },
+      (callback) => {
+        callback(null, '3');
+      },
+    ], (error, results) => {
+      console.log(results);
+    });
   },
   methods: {
     getSteamID() {
       axios.get(`http://localhost:3000/steamid?username=${this.$route.params.username}`)
       .then((response) => {
-        console.log(response.request.response);
-        this.steamUserID = JSON.parse(response.request.response);
+        this.steamUserID = response.data;
       })
       .catch((error) => {
         console.log(error);
       });
-    },
-    getCSGOStats() {
-
     },
   },
   data() {
     return {
       msg: this.$route.params.username,
       steamUserID: 'holder',
+      steamUserStats: [],
       url: `https://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=${steamAPIKey}&vanityurl=${this.$route.params.username}`,
     };
   },
@@ -40,22 +59,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+  .profile-text{
+    margin-top: 10px;
+  }
+  .profile-avatar{
+    height: 60px;
+    border-radius: 360px;
+  }
 </style>
